28db4e18b7e8b95aa8845482666d1dbd48b7c01a
Sharif101 <sharif009009@gmail.com>
2022-09-29 13:20:31 +0600

    added

--- src/Componets/Details/Details.js
+++ src/Componets/Details/Details.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Details.css';
 
 const Details = (props) => {
     let {work} = props;
@@ -9,6 +10,15 @@ const Details = (props) => {
     }
     return (
         <div className='details'>
+            <div className="info">
+                <h2>MD. Sharif</h2>
+                <p>Sydney, Australia</p>
+            </div>
+            <div className="bmi">
+                <h3>73kg <p>Weight</p></h3>
+                <h3>5.9 <p>Height</p></h3>
+                <h3>23 <p>Age</p></h3>
+            </div>
             <h3>Exercise time: {time} seconds</h3>
         </div>
     );
